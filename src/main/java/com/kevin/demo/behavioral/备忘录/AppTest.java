61ba79bedac3b87211948a8ab6c61c4cd06156d6
ylzyd12345 <290378941@qq.com>
2015-12-17 01:59:16 +0800

    add memento pattern

--- src/main/java/com/kevin/demo/behavioral/备忘录/AppTest.java
+++ src/main/java/com/kevin/demo/behavioral/备忘录/AppTest.java
@@ -1,4 +1,4 @@
-package com.kevin.demo.behavioral.访问者;
+package com.kevin.demo.behavioral.备忘录;
 
 public class AppTest {
 	public static void main(String[] args) {
---
+++ src/main/java/com/kevin/demo/behavioral/备忘录/Memento.java
@@ -0,0 +1,23 @@
+package com.kevin.demo.behavioral.备忘录;
+
+public class Memento {
+	private int bloodFlow;
+	private int magicPoint;
+	
+	public Memento(int bloodFlow, int magicPoint) {
+		this.bloodFlow = bloodFlow;
+		this.magicPoint = magicPoint;
+	}
+	public int getBloodFlow() {
+		return bloodFlow;
+	}
+	public void setBloodFlow(int bloodFlow) {
+		this.bloodFlow = bloodFlow;
+	}
+	public int getMagicPoint() {
+		return magicPoint;
+	}
+	public void setMagicPoint(int magicPoint) {
+		this.magicPoint = magicPoint;
+	}
+}
